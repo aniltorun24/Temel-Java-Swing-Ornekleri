184fb1b6dfd8ee9c1bb399ad10b2e09b90c033e8
aniltorun24 <torunanil05@gmail.com>
2024-04-14 15:52:45 +0300

    swing-Demo

---
+++ swingDemos/firstSwingDemos/src/main/java/com/mycompany/firstswingdemos/FirstSwingDemos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstswingdemos;
+
+/**
+ *
+ * @author Anıl Enes torun
+ */
+public class FirstSwingDemos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ swingDemos/swingDemo/src/main/java/com/mycompany/swingdemo/SwingDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.swingdemo;
+
+/**
+ *
+ * @author Anıl Enes torun
+ */
+public class SwingDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
